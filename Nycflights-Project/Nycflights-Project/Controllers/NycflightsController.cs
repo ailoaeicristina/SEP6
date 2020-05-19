ff5dda162dc4e0068d91f0f01047af97c4f5b9be
Cristina Ailoaei <ailoaeicristina@yahoo.com>
2020-05-19 23:53:02 +0200

    Exposed API for flights per month for 3 origins

--- Nycflights-Project/Nycflights-Project/Controllers/NycflightsController.cs
+++ Nycflights-Project/Nycflights-Project/Controllers/NycflightsController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
 using Nycflights_Project.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +20,48 @@ namespace Nycflights_Project.Controllers
                 {7, "Jul"},  {8, "Aug"},  {9, "Sep"},  {10, "Oct"},  {11, "Nov"},  {12, "Dec"}};
 
             return context.Flights.Select(f => f.Month).ToList().GroupBy(m => m).ToDictionary(g => monthsByNumber[g.Key], g => g.Count());
+        }
+
+        // GET: api/Nycflights/FlightsPerMonthForJFK
+        [HttpGet("[action]")]
+        public Dictionary<string, int> FlightsPerMonthForJFK()
+        {
+            var context = new Nycflights13DBContext();
+
+            Dictionary<int, string> monthsByNumber = new Dictionary<int, string>()
+            { {1, "Jan" }, {2, "Feb"}, {3, "Mar"},  {4, "Apr"},  {5, "May"},  {6, "Jun"},
+                {7, "Jul"},  {8, "Aug"},  {9, "Sep"},  {10, "Oct"},  {11, "Nov"},  {12, "Dec"}};
+
+            return context.Flights.Where(f => !string.IsNullOrEmpty(f.Origin) && f.Origin.Equals("JFK")).Select(f => f.Month).ToList()
+                .GroupBy(m => m).ToDictionary(g => monthsByNumber[g.Key], g => g.Count());
+        }
+
+        // GET: api/Nycflights/FlightsPerMonthForEWR
+        [HttpGet("[action]")]
+        public Dictionary<string, int> FlightsPerMonthForEWR()
+        {
+            var context = new Nycflights13DBContext();
+
+            Dictionary<int, string> monthsByNumber = new Dictionary<int, string>()
+            { {1, "Jan" }, {2, "Feb"}, {3, "Mar"},  {4, "Apr"},  {5, "May"},  {6, "Jun"},
+                {7, "Jul"},  {8, "Aug"},  {9, "Sep"},  {10, "Oct"},  {11, "Nov"},  {12, "Dec"}};
+
+            return context.Flights.Where(f => !string.IsNullOrEmpty(f.Origin) && f.Origin.Equals("EWR")).Select(f => f.Month).ToList()
+                .GroupBy(m => m).ToDictionary(g => monthsByNumber[g.Key], g => g.Count());
+        }
+
+        // GET: api/Nycflights/FlightsPerMonthForLGA
+        [HttpGet("[action]")]
+        public Dictionary<string, int> FlightsPerMonthForLGA()
+        {
+            var context = new Nycflights13DBContext();
+
+            Dictionary<int, string> monthsByNumber = new Dictionary<int, string>()
+            { {1, "Jan" }, {2, "Feb"}, {3, "Mar"},  {4, "Apr"},  {5, "May"},  {6, "Jun"},
+                {7, "Jul"},  {8, "Aug"},  {9, "Sep"},  {10, "Oct"},  {11, "Nov"},  {12, "Dec"}};
+
+            return context.Flights.Where(f => !string.IsNullOrEmpty(f.Origin) && f.Origin.Equals("LGA")).Select(f => f.Month).ToList()
+                .GroupBy(m => m).ToDictionary(g => monthsByNumber[g.Key], g => g.Count());
         }
 
     }
