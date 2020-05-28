da905690bc0aeeb897d65b93a1c8cd3350b3d6f0
Dragos Sirbu <dragsirbu@gmail.com>
2020-05-28 19:48:49 +0200

    Added Bootstrap, Angular Material, Resolved Dependencies, Code Cleanup

--- Nycflights-Project/Nycflights-Project/ClientApp/src/app/app.module.ts
+++ Nycflights-Project/Nycflights-Project/ClientApp/src/app/app.module.ts
@@ -7,14 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MaterialModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
--- Nycflights-Project/Nycflights-Project/ClientApp/src/app/material/material.module.ts
+++ Nycflights-Project/Nycflights-Project/ClientApp/src/app/material/material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 const MaterialComponents = [
-    MatSidenavModule
+    MatSidenavModule,
+    MatExpansionModule
 ];
 
 @NgModule({
