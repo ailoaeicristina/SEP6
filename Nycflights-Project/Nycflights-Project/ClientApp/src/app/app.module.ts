fe5328acb02e97fa87d0d82b70acf9a74027ba65
Dragos Sirbu <dragsirbu@gmail.com>
2020-05-28 20:35:57 +0200

    Created Sidenav

--- Nycflights-Project/Nycflights-Project/ClientApp/src/app/app.module.ts
+++ Nycflights-Project/Nycflights-Project/ClientApp/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
     declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { MaterialModule } from './material/material.module';
         AppRoutingModule,
         BrowserAnimationsModule,
         MaterialModule,
+        MatListModule
     ],
     providers: [],
     bootstrap: [AppComponent],
--- Nycflights-Project/Nycflights-Project/ClientApp/src/app/material/material.module.ts
+++ Nycflights-Project/Nycflights-Project/ClientApp/src/app/material/material.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 const MaterialComponents = [
     MatSidenavModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatIconModule
 ];
 
 @NgModule({
-    imports: [ MaterialComponents],
-    exports: [ MaterialComponents],
+    imports: [MaterialComponents],
+    exports: [MaterialComponents],
 })
 export class MaterialModule {}
