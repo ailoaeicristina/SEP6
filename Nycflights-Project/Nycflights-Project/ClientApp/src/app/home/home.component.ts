3a831471cace19c96148306328f5ea64197424fb
Cristina Ailoaei <ailoaeicristina@yahoo.com>
2020-05-28 09:28:44 +0200

    Retrieved value from component

--- Nycflights-Project/Nycflights-Project/ClientApp/src/app/home/home.component.ts
+++ Nycflights-Project/Nycflights-Project/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Dictionary } from 'lodash';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  public flightsPerMonth: FlightsPerMonth
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<FlightsPerMonth>(baseUrl + 'api/Nycflights/FlightsPerMonth').subscribe(result => {
+      this.flightsPerMonth = result;
+    }, error => console.error(error));
+  }
+}
+
+interface FlightsPerMonth {
+  connection: Map<string, number>;
 }
