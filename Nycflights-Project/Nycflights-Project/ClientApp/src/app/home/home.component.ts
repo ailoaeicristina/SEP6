2a18ad85dc7584c4e38c1e3636ab0c58ba63980e
Cristina Ailoaei <ailoaeicristina@yahoo.com>
2020-05-29 12:54:39 +0200

    Added rendering for charts

--- Nycflights-Project/Nycflights-Project/ClientApp/src/app/home/home.component.ts
+++ Nycflights-Project/Nycflights-Project/ClientApp/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, AfterViewInit, Inject } from '@angular/core';
 import * as CanvasJS from './canvasjs.min';
 import { HttpClient } from '@angular/common/http';
 
@@ -7,11 +7,15 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html'
 })
 
-export class HomeComponent{
+export class HomeComponent implements AfterViewInit {
 
   public flightsPerMonth: Map<string, number> = new Map<string, number>();
+  public http: HttpClient;
+  public baseUrl: string;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.http = http;
+    this.baseUrl = baseUrl;
     // Feature 1 - Flights per month
     http.get<Map<string, number>>(baseUrl + 'api/Nycflights/FlightsPerMonth').subscribe(result => {
 
@@ -19,7 +23,6 @@ export class HomeComponent{
 
       Object.keys(result).forEach(function (key) {
         dataPoints.push({ label: key, y: result[key] })
-        console.log(key + ":" + result[key]);
       });
 
       let chart = new CanvasJS.Chart("chartContainer1", {
@@ -44,40 +47,37 @@ export class HomeComponent{
 
     }, error => console.error(error));
 
-    //Feature 2 - Flights per month from origins
-    let dataPointsJFK = [];
-    http.get<Map<string, number>>(baseUrl + 'api/Nycflights/FlightsPerMonthForJFK').subscribe(result => {
+    //Feature 3 - Flights to top 10 destinations
+    let dataPointsJFKToDestinations = [];
+    http.get<Map<string, number>>(baseUrl + 'api/Nycflights/FlightsToTopTenDestinationsFromJFK').subscribe(result => {
 
       Object.keys(result).forEach(function (key) {
-        dataPointsJFK.push({ label: key, y: result[key] })
-        console.log(key + ":" + result[key]);
+        dataPointsJFKToDestinations.push({ label: key, y: result[key] })
       });
     }, error => console.error(error));
 
-    let dataPointsEWR = [];
-    http.get<Map<string, number>>(baseUrl + 'api/Nycflights/FlightsPerMonthForEWR').subscribe(result => {
+    let dataPointsEWRToDestinations = [];
+    http.get<Map<string, number>>(baseUrl + 'api/Nycflights/FlightsToTopTenDestinationsFromEWR').subscribe(result => {
 
       Object.keys(result).forEach(function (key) {
-        dataPointsEWR.push({ label: key, y: result[key] })
-        console.log(key + ":" + result[key]);
+        dataPointsEWRToDestinations.push({ label: key, y: result[key] })
       });
     }, error => console.error(error));
 
-    let dataPointsLGA = [];
-    http.get<Map<string, number>>(baseUrl + 'api/Nycflights/FlightsPerMonthForLGA').subscribe(result => {
+    let dataPointsLGAToDestinations = [];
+    http.get<Map<string, number>>(baseUrl + 'api/Nycflights/FlightsToTopTenDestinationsFromLGA').subscribe(result => {
 
       Object.keys(result).forEach(function (key) {
-        dataPointsLGA.push({ label: key, y: result[key] })
-        console.log(key + ":" + result[key]);
+        dataPointsLGAToDestinations.push({ label: key, y: result[key] })
       });
 
-      let chartFrequency = new CanvasJS.Chart("chartContainer2Frequency", {
+      let chartFrequency = new CanvasJS.Chart("chartContainer3", {
         animationEnabled: true,
         title: {
-          text: "Number of flights per month - frequency"
+          text: "Number of flights to top 10 destinations from origins"
         },
         axisX: {
-          title: "Month",
+          title: "Destination",
           interval: 1
         },
         axisY: {
@@ -87,105 +87,183 @@ export class HomeComponent{
           type: "column",
           legendText: "JFK",
           showInLegend: true,
-          dataPoints: dataPointsJFK,
+          dataPoints: dataPointsJFKToDestinations,
           color: "#2E86C1"
-          },
-          {
-            type: "column",
-            legendText: "EWR",
-            showInLegend: true,
-            dataPoints: dataPointsEWR,
-            color: "#C13B2E"
-          },
-          {
-            type: "column",
-            legendText: "LGA",
-            showInLegend: true,
-            dataPoints: dataPointsLGA,
-            color: "#2EC146"
-          },
+        },
+        {
+          type: "column",
+          legendText: "EWR",
+          showInLegend: true,
+          dataPoints: dataPointsEWRToDestinations,
+          color: "#C13B2E"
+        },
+        {
+          type: "column",
+          legendText: "LGA",
+          showInLegend: true,
+          dataPoints: dataPointsLGAToDestinations,
+          color: "#2EC146"
+        },
         ]
       });
       chartFrequency.render();
 
-      let chartStacked = new CanvasJS.Chart("chartContainer2Stacked", {
-        animationEnabled: true,
-        title: {
-          text: "Number of flights per month - stacked"
-        },
-        axisX: {
-          title: "Month",
-          interval: 1
-        },
-        axisY: {
-          title: "Number of flights"
-        },
-        data: [{
-          type: "stackedColumn",
-          legendText: "JFK",
-          showInLegend: true,
-          dataPoints: dataPointsJFK,
-          color: "#2E86C1"
-        },
-        {
-          type: "stackedColumn",
-          legendText: "EWR",
-          showInLegend: true,
-          dataPoints: dataPointsEWR,
-          color: "#C13B2E"
-        },
-        {
-          type: "stackedColumn",
-          legendText: "LGA",
-          showInLegend: true,
-          dataPoints: dataPointsLGA,
-          color: "#2EC146"
-        },
-        ]
+    }, error => console.error(error)); 
+  }
+
+  ngAfterViewInit() {
+    //Feature 2 - Flights per month from origins
+    let dataPointsJFK = [];
+    let dataPointsEWR = [];
+    let dataPointsLGA = [];
+
+    let chartFrequency = new CanvasJS.Chart("chartContainer2Frequency", {
+      animationEnabled: true,
+      title: {
+        text: "Number of flights per month - frequency"
+      },
+      axisX: {
+        title: "Month",
+        interval: 1
+      },
+      axisY: {
+        title: "Number of flights"
+      },
+      data: [{
+        type: "column",
+        legendText: "JFK",
+        showInLegend: true,
+        dataPoints: dataPointsJFK,
+        color: "#2E86C1"
+      },
+      {
+        type: "column",
+        legendText: "EWR",
+        showInLegend: true,
+        dataPoints: dataPointsEWR,
+        color: "#C13B2E"
+      },
+      {
+        type: "column",
+        legendText: "LGA",
+        showInLegend: true,
+        dataPoints: dataPointsLGA,
+        color: "#2EC146"
+      },
+      ]
+    });
+
+    let chartStacked = new CanvasJS.Chart("chartContainer2Stacked", {
+      animationEnabled: true,
+      title: {
+        text: "Number of flights per month - stacked"
+      },
+      axisX: {
+        title: "Month",
+        interval: 1
+      },
+      axisY: {
+        title: "Number of flights"
+      },
+      data: [{
+        type: "stackedColumn",
+        legendText: "JFK",
+        showInLegend: true,
+        dataPoints: dataPointsJFK,
+        color: "#2E86C1"
+      },
+      {
+        type: "stackedColumn",
+        legendText: "EWR",
+        showInLegend: true,
+        dataPoints: dataPointsEWR,
+        color: "#C13B2E"
+      },
+      {
+        type: "stackedColumn",
+        legendText: "LGA",
+        showInLegend: true,
+        dataPoints: dataPointsLGA,
+        color: "#2EC146"
+      },
+      ]
+    });
+
+    let chartStackedPercentage = new CanvasJS.Chart("chartContainer2StackedPercentage", {
+      animationEnabled: true,
+      title: {
+        text: "Number of flights per month - stacked percentage"
+      },
+      axisX: {
+        title: "Month",
+        interval: 1
+      },
+      axisY: {
+        title: "Number of flights",
+        suffix: "%"
+      },
+      data: [{
+        type: "stackedColumn100",
+        legendText: "JFK",
+        toolTipContent: "{y} (#percent%)",
+        showInLegend: true,
+        dataPoints: dataPointsJFK,
+        color: "#2E86C1"
+      },
+      {
+        type: "stackedColumn100",
+        legendText: "EWR",
+        toolTipContent: "{y} (#percent%)",
+        showInLegend: true,
+        dataPoints: dataPointsEWR,
+        color: "#C13B2E"
+      },
+      {
+        type: "stackedColumn100",
+        legendText: "LGA",
+        toolTipContent: "{y} (#percent%)",
+        showInLegend: true,
+        dataPoints: dataPointsLGA,
+        color: "#2EC146"
+      },
+      ]
+    });
+
+    chartFrequency.render();
+    chartStacked.render();
+    chartStackedPercentage.render();
+
+    this.http.get<Map<string, number>>(this.baseUrl + 'api/Nycflights/FlightsPerMonthForJFK').subscribe(result => {
+
+      Object.keys(result).forEach(function (key) {
+        dataPointsJFK.push({ label: key, y: result[key] })
       });
+
+      chartFrequency.render();
       chartStacked.render();
-
-      let chartStackedPercentage = new CanvasJS.Chart("chartContainer2StackedPercentage", {
-        animationEnabled: true,
-        title: {
-          text: "Number of flights per month - stacked percentage"
-        },
-        axisX: {
-          title: "Month",
-          interval: 1
-        },
-        axisY: {
-          title: "Number of flights",
-          suffix:"%"
-        },
-        data: [{
-          type: "stackedColumn100",
-          legendText: "JFK",
-          toolTipContent: "{y} (#percent%)",
-          showInLegend: true,
-          dataPoints: dataPointsJFK,
-          color: "#2E86C1"
-        },
-        {
-          type: "stackedColumn100",
-          legendText: "EWR",
-          toolTipContent: "{y} (#percent%)",
-          showInLegend: true,
-          dataPoints: dataPointsEWR,
-          color: "#C13B2E"
-        },
-        {
-          type: "stackedColumn100",
-          legendText: "LGA",
-          toolTipContent: "{y} (#percent%)",
-          showInLegend: true,
-          dataPoints: dataPointsLGA,
-          color: "#2EC146"
-        },
-        ]
-      });
       chartStackedPercentage.render();
+    }, error => console.error(error));
 
+    this.http.get<Map<string, number>>(this.baseUrl + 'api/Nycflights/FlightsPerMonthForEWR').subscribe(result => {
+
+      Object.keys(result).forEach(function (key) {
+        dataPointsEWR.push({ label: key, y: result[key] })
+      });
+
+      chartFrequency.render();
+      chartStacked.render();
+      chartStackedPercentage.render();
+    }, error => console.error(error));
+
+    this.http.get<Map<string, number>>(this.baseUrl + 'api/Nycflights/FlightsPerMonthForLGA').subscribe(result => {
+
+      Object.keys(result).forEach(function (key) {
+        dataPointsLGA.push({ label: key, y: result[key] })
+      });
+
+      chartFrequency.render();
+      chartStacked.render();
+      chartStackedPercentage.render();
     }, error => console.error(error));
   }
 }
