4c5b2641327f3505b6b6ce350a91eee558594ba5
Cristina Ailoaei <ailoaeicristina@yahoo.com>
2020-05-30 12:50:50 +0200

    Resized charts

--- Nycflights-Project/Nycflights-Project/ClientApp/src/app/flights/flights.component.ts
+++ Nycflights-Project/Nycflights-Project/ClientApp/src/app/flights/flights.component.ts
@@ -1,6 +1,6 @@
 import { Component, AfterViewInit, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import * as CanvasJS from './canvasjs.min';
+import * as CanvasJS from '../../assets/canvasjs.min.js';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
@@ -15,13 +15,9 @@ export class FlightsComponent implements AfterViewInit {
 
   public flightsToTopTenDestinations: Map<string, number>;
   public meanAirtimeForOrigins: Map<string, string>;
-  public weatherObservationsForOrigins: Map<string, number>;
   public meanDepartureAndArrivalDelaysForJFK: Map<string, string>;
   public meanDepartureAndArrivalDelaysForEWR: Map<string, string>;
   public meanDepartureAndArrivalDelaysForLGA: Map<string, string>;
-  public manufacturersMoreThanTwoHundredPlanes: Map<string, number>;
-  public flightsForManufacturersMoreThanTwoHundredPlanes: Map<string, number>;
-  public planesForEachAirbusModel: Map<string, number>;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     this.http = http;
@@ -42,32 +38,8 @@ export class FlightsComponent implements AfterViewInit {
     //Feature 4 - Mean airtime for origins
     this.loadMeanAirtimeForOrigins();
 
-    //Feature 5 - Weather observations for origins
-    this.loadWeatherObservationsForOrigins();
-
-    //Feature 6 - Temperature attributes for origins
-    this.loadTemperatureAttributesForOrigins();
-
-    //Feature 7 - Temperatures for JFK
-    this.loadTemperaturesForJFK();
-
-    //Feature 8 - Daily mean temperature for JFK
-    this.loadDailyMeanTemperatureForJFK();
-
-    //Feature 9 - Daily mean temperature for origins
-    this.loadDailyMeanTemperatureForOrigins();
-
     //Feature 10 - Departure and arrival delays for origins
     this.loadDepartureAndArrivalDelaysForOrigins();
-
-    //Feature 11 - Manufacturers with more than 200 planes
-    this.loadManufacturersMoreThanTwoHundredPlanes();
-
-    //Feature 12 - Flights for manufacturers with more than 200 planes
-    this.loadFlightsForManufacturersWithMoreThanTwoHundredPlanes();
-
-    //Feature 13 - Planes for each Airbus model
-    this.loadPlanesForEachAirbusModel();
   }
 
   loadFlightsPerMonth() {
@@ -339,222 +311,6 @@ export class FlightsComponent implements AfterViewInit {
     }, error => console.error(error));
   }
 
-  loadWeatherObservationsForOrigins() {
-    this.http.get<Map<string, number>>(this.baseUrl + 'api/Nycflights/WeatherObservationsForOrigins').subscribe(result => {
-      this.weatherObservationsForOrigins = result;
-    }, error => console.error(error));
-  }
-
-  loadTemperatureAttributesForOrigins() {
-    let dataPointsJFK = [];
-    let dataPointsEWR = [];
-    let dataPointsLGA = [];
-
-    let chart = new CanvasJS.Chart("chartContainer6", {
-      animationEnabled: true,
-      title: {
-        text: "Temperature attributes for origins"
-      },
-      axisX: {
-        title: "Date",
-      },
-      axisY: {
-        title: "Temperature in Celsius"
-      },
-      data: [{
-        type: "scatter",
-        legendText: "JFK",
-        showInLegend: true,
-        dataPoints: dataPointsJFK,
-        color: "#2E86C1"
-      },
-      {
-        type: "scatter",
-        legendText: "EWR",
-        showInLegend: true,
-        dataPoints: dataPointsEWR,
-        color: "#C13B2E"
-      },
-      {
-        type: "scatter",
-        legendText: "LGA",
-        showInLegend: true,
-        dataPoints: dataPointsLGA,
-        color: "#2EC146"
-      },
-      ]
-    });
-
-    chart.render();
-
-    this.http.get<Map<Date, number>>(this.baseUrl + 'api/Nycflights/TemperatureInCelsiusForJFK').subscribe(result => {
-
-      Object.keys(result).forEach(function (key) {
-        dataPointsJFK.push({ label: key, y: result[key] })
-      });
-
-      chart.render();
-    }, error => console.error(error));
-
-    this.http.get<Map<Date, number>>(this.baseUrl + 'api/Nycflights/TemperatureInCelsiusForEWR').subscribe(result => {
-
-      Object.keys(result).forEach(function (key) {
-        dataPointsEWR.push({ label: key, y: result[key] })
-      });
-
-      chart.render();
-    }, error => console.error(error));
-
-    this.http.get<Map<Date, number>>(this.baseUrl + 'api/Nycflights/TemperatureInCelsiusForLGA').subscribe(result => {
-
-      Object.keys(result).forEach(function (key) {
-        dataPointsLGA.push({ label: key, y: result[key] })
-      });
-
-      chart.render();
-    }, error => console.error(error));
-  }
-
-  loadTemperaturesForJFK() {
-    let dataPointsJFK = [];
-
-    let chart = new CanvasJS.Chart("chartContainer7", {
-      animationEnabled: true,
-      title: {
-        text: "Temperatures registered for JFK"
-      },
-      axisX: {
-        title: "Date",
-      },
-      axisY: {
-        title: "Temperature in Celsius"
-      },
-      data: [{
-        type: "scatter",
-        legendText: "JFK",
-        showInLegend: true,
-        dataPoints: dataPointsJFK,
-        color: "#2E86C1"
-      }]
-    });
-
-    chart.render();
-
-    this.http.get<Map<Date, number>>(this.baseUrl + 'api/Nycflights/TemperatureInCelsiusForJFK').subscribe(result => {
-
-      Object.keys(result).forEach(function (key) {
-        dataPointsJFK.push({ label: key, y: result[key] })
-      });
-
-      chart.render();
-    }, error => console.error(error));
-  }
-
-  loadDailyMeanTemperatureForJFK() {
-    let dataPointsJFK = [];
-
-    let chart = new CanvasJS.Chart("chartContainer8", {
-      animationEnabled: true,
-      title: {
-        text: "Daily mean temperature for JFK"
-      },
-      axisX: {
-        title: "Date",
-      },
-      axisY: {
-        title: "Mean temperature"
-      },
-      data: [{
-        type: "scatter",
-        legendText: "JFK",
-        showInLegend: true,
-        dataPoints: dataPointsJFK,
-        color: "#2E86C1"
-      }]
-    });
-
-    chart.render();
-
-    this.http.get<Map<string, number>>(this.baseUrl + 'api/Nycflights/DailyMeanTempInCelsiusForJFK').subscribe(result => {
-
-      Object.keys(result).forEach(function (key) {
-        dataPointsJFK.push({ label: key, y: result[key] })
-      });
-
-      chart.render();
-    }, error => console.error(error));
-  }
-
-  loadDailyMeanTemperatureForOrigins() {
-    let dataPointsJFK = [];
-    let dataPointsEWR = [];
-    let dataPointsLGA = [];
-
-    let chart = new CanvasJS.Chart("chartContainer9", {
-      animationEnabled: true,
-      title: {
-        text: "Daily mean temperature for origins"
-      },
-      axisX: {
-        title: "Date",
-      },
-      axisY: {
-        title: "Mean temperature"
-      },
-      data: [{
-        type: "scatter",
-        legendText: "JFK",
-        showInLegend: true,
-        dataPoints: dataPointsJFK,
-        color: "#2E86C1"
-      },
-      {
-        type: "scatter",
-        legendText: "EWR",
-        showInLegend: true,
-        dataPoints: dataPointsEWR,
-        color: "#C13B2E"
-      },
-      {
-        type: "scatter",
-        legendText: "LGA",
-        showInLegend: true,
-        dataPoints: dataPointsLGA,
-        color: "#2EC146"
-      },
-      ]
-    });
-
-    chart.render();
-
-    this.http.get<Map<string, number>>(this.baseUrl + 'api/Nycflights/DailyMeanTempInCelsiusForJFK').subscribe(result => {
-
-      Object.keys(result).forEach(function (key) {
-        dataPointsJFK.push({ label: key, y: result[key] })
-      });
-
-      chart.render();
-    }, error => console.error(error));
-
-    this.http.get<Map<string, number>>(this.baseUrl + 'api/Nycflights/DailyMeanTempInCelsiusForEWR').subscribe(result => {
-
-      Object.keys(result).forEach(function (key) {
-        dataPointsEWR.push({ label: key, y: result[key] })
-      });
-
-      chart.render();
-    }, error => console.error(error));
-
-    this.http.get<Map<string, number>>(this.baseUrl + 'api/Nycflights/DailyMeanTempInCelsiusForLGA').subscribe(result => {
-
-      Object.keys(result).forEach(function (key) {
-        dataPointsLGA.push({ label: key, y: result[key] })
-      });
-
-      chart.render();
-    }, error => console.error(error));
-  }
-
   loadDepartureAndArrivalDelaysForOrigins() {
     this.http.get<Map<string, string>>(this.baseUrl + 'api/Nycflights/MeanDepartureAndArrivalDelayForJFK').subscribe(result => {
       this.meanDepartureAndArrivalDelaysForJFK = result;
@@ -569,23 +325,6 @@ export class FlightsComponent implements AfterViewInit {
     }, error => console.error(error));
   }
 
-  loadManufacturersMoreThanTwoHundredPlanes() {
-    this.http.get<Map<string, number>>(this.baseUrl + 'api/Nycflights/ManufacturersMoreThanTwoHundredPlanes').subscribe(result => {
-      this.manufacturersMoreThanTwoHundredPlanes = result;
-    }, error => console.error(error));
-  }
-
-  loadFlightsForManufacturersWithMoreThanTwoHundredPlanes() {
-    this.http.get<Map<string, number>>(this.baseUrl + 'api/Nycflights/FlightsManufacturersMoreThanTwoHundredPlanes').subscribe(result => {
-      this.flightsForManufacturersMoreThanTwoHundredPlanes = result;
-    }, error => console.error(error));
-  }
-
-  loadPlanesForEachAirbusModel() {
-    this.http.get<Map<string, number>>(this.baseUrl + 'api/Nycflights/PlanesforAirbus').subscribe(result => {
-      this.planesForEachAirbusModel = result;
-    }, error => console.error(error));
-  }
 }
 
 @Pipe({ name: 'getValues' })
